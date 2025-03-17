7394989065c4c3639e03843b472544c62ae1b810
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2025-03-17 23:01:55 +0530

    🔥 default app routes url changes and cors Handler

--- routes/admin.go
+++ routes/admin.go
@@ -3,11 +3,18 @@ package routes
 import (
 	controllers "restpad/restpad-service/controllers/app"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func AdminRouter(router *gin.Engine) {
-	admin := router.Group("/admin")
+	// CORS Config
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+
+	router.Use(cors.New(config))
+
+	admin := router.Group("app/api/v1")
 	{
 		admin.POST("/post", controllers.PostRequestHandler())
 	}
